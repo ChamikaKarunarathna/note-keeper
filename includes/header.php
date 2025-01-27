596b2e73dbcdc9ceeaf9264729afad625d7dceba
Chamika Karunarathna <cklkarunarathnavta@gmail.com>
2025-01-27 22:34:11 +0530

    do1: Create Main Layout and UI

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<nav class="flex items-center justify-between p-6 lg:px-8 max-w-screen-xl mx-auto" aria-label="Global">
+    <div class="flex lg:flex-1">
+        <a href="?page=home" class="-m-1.5 p-1.5 flex space-x-3 rtl:space-x-reverse">
+            <span class="sr-only">NoteKeeper</span>
+            <img
+                src="./assets/images/note-keeper-logo.png"
+                class="h-8 w-auto"
+                alt="NoteKeeper Logo" />
+            <span class="hidden lg:flex self-center text-2xl font-semibold whitespace-nowrap">NoteKeeper</span>
+        </a>
+    </div>
+    <!-- <div class="hidden lg:flex lg:gap-x-12">
+            <a href="#" class="text-sm/6 font-semibold text-gray-900">Product</a>
+            <a href="#" class="text-sm/6 font-semibold text-gray-900">Features</a>
+            <a href="#" class="text-sm/6 font-semibold text-gray-900">Marketplace</a>
+            <a href="#" class="text-sm/6 font-semibold text-gray-900">Company</a>
+        </div> -->
+    <div class="flex flex-1 justify-end">
+        <a href="?page=login" class="text-sm/6 font-semibold text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+    </div>
+</nav>
